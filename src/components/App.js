07da8d4f6509ad21e7f71680e187d8034b812c70
Heneros <punk60826@gmail.com>
2022-01-20 02:15:36 +0200

    changing language and color btn

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext  from '../contexts/ColorContext';
+
 
 class App extends React.Component{
-    state = { language: 'english'};
+   static contextType = LanguageContext;
+    state = { language: 'russian'};
 
     onLanguageChange = language =>{
         this.setState({ language });
     }
 
     render(){
+        const text = this.context === 'russian' ? 'Выберете язык' : ' Select a language';        
         return <div className='ui container'>
             <div>
-                Select a language:
+                {text}:
                 <i className='flag us' onClick={() => this.onLanguageChange('english')}/>
                 <i className='flag ru'  onClick={() => this.onLanguageChange('russian')}/>
             </div>
-            <UserCreate/>
+            <ColorContext.Provider value="red">
+            <LanguageContext.Provider
+            value={this.state.language}>
+            <UserCreate />
+            </LanguageContext.Provider>
+            </ColorContext.Provider>
         </div>
     }
 }
--- src/components/UserCreate.js
+++ src/components/UserCreate.js
@@ -6,6 +6,7 @@ const UserCreate = () =>{
     return (
         <div className='ui form'>
             <Field />
+            
             <Button />
         </div>
     )
