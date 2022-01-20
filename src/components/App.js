289fdd43faff61331ba925f12d440aad5d60b30d
Heneros <punk60826@gmail.com>
2022-01-21 01:29:54 +0200

    plemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,20 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import {LanguageStore} from '../contexts/LanguageContext';
 import ColorContext  from '../contexts/ColorContext';
-
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component{
-   static contextType = LanguageContext;
-    state = { language: 'russian'};
-
-    onLanguageChange = language =>{
-        this.setState({ language });
-    }
 
     render(){
-        const text = this.context === 'russian' ? 'Выберете язык' : ' Select a language';        
+        // const text = this.context === 'russian' ? 'Выберете язык' : ' Select a language';        
         return <div className='ui container'>
-            <div>
-                {text}:
-                <i className='flag us' onClick={() => this.onLanguageChange('english')}/>
-                <i className='flag ru'  onClick={() => this.onLanguageChange('russian')}/>
-            </div>
+            <LanguageStore>
+            <LanguageSelector />
             <ColorContext.Provider value="red">
-            <LanguageContext.Provider
-            value={this.state.language}>
             <UserCreate />
-            </LanguageContext.Provider>
             </ColorContext.Provider>
+            </LanguageStore>
         </div>
     }
 }
